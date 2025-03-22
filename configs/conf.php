45fbfda12151b9ecdf34f38b140aa0c18240ff2d
AHMED HASSAN <151830489+AHMED1CB@users.noreply.github.com>
2025-03-22 21:27:36 +0000

    Update conf.php

--- configs/conf.php
+++ configs/conf.php
@@ -5,10 +5,10 @@ return [
      "database" => [
           
           "PRO" => [
-          'user' => 'ahmed1cb', // sql12761693
-          'pass' => 'ah12345500', // tbJlEZeYtM
-          'database' => 'ahmed1cb_chatsync', // sql12761693
-          'host' => 'mysql-ahmed1cb.alwaysdata.net' // sql12.freesqldatabase.com
+          'user' => 'root', 
+          'pass' => '',
+          'database' => 'chatsync',
+          'host' => 'localhost'
           ] ,
           
           "DEV" => [
